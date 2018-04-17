677225176fd8fa36b334197df4072f05abf4c4d3
Farzam Noori <farzamn80@gmail.com>
2018-04-17 14:12:27 -0600

    fresh start

--- config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'api_address': 'localhost:3000'
-}
--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-export const count = state => state.count
-
-export const dealershipLoggedIn = state => state.dealershipLoggedIn
-export const isLoggedIn = state => {
-    return state.isLoggedIn
-}

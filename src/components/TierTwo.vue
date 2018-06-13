0fec264893a4e6dda9b11d23be5cf87df901bae9
Farzam Noori <farzamn80@gmail.com>
2018-06-13 17:58:19 -0600

    Tier 2 horizontal cards playing hard to get

--- src/components/TierTwo.vue
+++ src/components/TierTwo.vue
@@ -2,10 +2,10 @@
     <div>
         <b-card>
             <img src="https://i.pinimg.com/originals/c2/57/b8/c257b847e55fd0572fb035ec04bb3a1e.jpg" alt="">
-            <div>
+            <div class="card-text">
+                <div class="card-title">Honda Civic</div>
                 div  This is some text within the default card block.
             </div>
-            
         </b-card>
     </div>
 </template>
@@ -24,15 +24,18 @@
 <style scoped>
     .card {
         flex-direction: row;
-        width: 50%;
-        margin: 0 auto;
+        width: 50rem;
+        margin: 2rem auto;
         box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.2);
+    }
+    .card-body {
+        padding: 0;
     }
     .card-body img {
         width: 30%;
         display: inline-block;
     }
-    .card-body div {
+    .card-text {
         display: inline-block;
     }
 </style>

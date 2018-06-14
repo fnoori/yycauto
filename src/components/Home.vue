700b3b59391141c37f4db6af311ebbf107aa15e6
Farzam Noori <farzamn80@gmail.com>
2018-06-14 17:20:39 -0600

    Changed how tier one components are populated

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home container-fluid">
-        <tier-one></tier-one>
+        <tier-one v-bind:carDetails="items"></tier-one>
         <tier-two></tier-two>
         <tier-three></tier-three>
     </div>
@@ -18,9 +18,71 @@
             'tier-three': TierThree
         },
         data() {
-            return {}
+            return {
+                items: [
+                    { 
+                        title: 'Honda Civic', 
+                        price: '$23,000',
+                        kilometres: '100,219',
+                        fuelType: 'Gas',
+                        transmission: 'Auto',
+                        locationLink: 'http://maps.google.com/?q=Village Honda',
+                        locationName: 'Village Honda',
+                        image: 'https://fitzdecarts.com/wp-content/uploads/2018/04/honda-civic-456.jpg'
+                    },
+                    { 
+                        title: 'Toyota Corola', 
+                        price: '$22,000',
+                        kilometres: '0',
+                        fuelType: 'Gas',
+                        transmission: 'Manual',
+                        locationLink: 'http://maps.google.com/?q=Country Hills Toyota',
+                        locationName: 'Country Hills Toyota',
+                        image: 'http://autowpaper.com/images/toyota-corolla-2015-sport-2.jpg'
+                    },
+                    { 
+                        title: 'Volkwagon Golf GTI', 
+                        price: '$35,000',
+                        kilometres: '2,000',
+                        fuelType: 'Gas',
+                        transmission: 'Auto',
+                        locationLink: 'http://maps.google.com/?q=Northland Volkswagon',
+                        locationName: 'Northland Volkswagon',
+                        image: 'https://www.conceptcarz.com/images/Volkswagen/2011-Volkswagen-Golf-GTi-Edition-35-01.jpg'
+                    },
+                    { 
+                        title: 'Ford Focus RS', 
+                        price: '$30,000',
+                        kilometres: '0',
+                        fuelType: 'Gas',
+                        transmission: 'Manual',
+                        locationLink: 'http://maps.google.com/?q=Northstar Ford',
+                        locationName: 'Northstar Ford',
+                        image: 'https://icdn-3.motor1.com/images/mgl/J2jkJ/s1/2016-ford-focus-rs.jpg'
+                    },
+                    { 
+                        title: 'Dodge Ram', 
+                        price: '$30,000',
+                        kilometres: '12,000',
+                        fuelType: 'Diesel',
+                        transmission: 'Auto',
+                        locationLink: 'http://maps.google.com/?q=Calgary Ford',
+                        locationName: 'Calgary Ford',
+                        image: 'https://images5.alphacoders.com/892/892407.jpg'
+                    },
+                    { 
+                        title: 'Nissan Sentra Spec-V', 
+                        price: '$40,000',
+                        kilometres: '0',
+                        fuelType: 'Gas',
+                        transmission: 'Manual',
+                        locationLink: 'http://maps.google.com/?q=Cochrane Nissan',
+                        locationName: 'Cochrane Nissan',
+                        image: 'https://i.pinimg.com/originals/c2/57/b8/c257b847e55fd0572fb035ec04bb3a1e.jpg'
+                    }
+                ]
+            }
         },
-
         methods: {}
     }
 </script>
--- src/components/TierOne.vue
+++ src/components/TierOne.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="tier-one">
-        <b-card-group class="tier-one-card" v-for="curr in items" v-bind:key="curr.message">
+    <div id="tierOne" class="tier-one">
+        <b-card-group class="tier-one-card" v-for="curr in carDetails" v-bind:key="curr.message">
             <b-card
                 no-body
                 :img-src="curr.image"
@@ -60,80 +60,20 @@
 
 <script>
     export default {
+        props: {
+            carDetails: {
+                type: Array,
+                required: true
+            }
+        },
         components: {},
         data() {
             return {
-                dealershipLocation: '123 ABC St. NW',
-
-                items: [
-                    { 
-                        title: 'Honda Civic', 
-                        price: '$23,000',
-                        kilometres: '100,219',
-                        fuelType: 'Gas',
-                        transmission: 'Auto',
-                        locationLink: 'http://maps.google.com/?q=Village Honda',
-                        locationName: 'Village Honda',
-                        image: 'https://fitzdecarts.com/wp-content/uploads/2018/04/honda-civic-456.jpg'
-                    },
-                    { 
-                        title: 'Toyota Corola', 
-                        price: '$22,000',
-                        kilometres: '0',
-                        fuelType: 'Gas',
-                        transmission: 'Manual',
-                        locationLink: 'http://maps.google.com/?q=Country Hills Toyota',
-                        locationName: 'Country Hills Toyota',
-                        image: 'http://autowpaper.com/images/toyota-corolla-2015-sport-2.jpg'
-                    },
-                    { 
-                        title: 'Volkwagon Golf GTI', 
-                        price: '$35,000',
-                        kilometres: '2,000',
-                        fuelType: 'Gas',
-                        transmission: 'Auto',
-                        locationLink: 'http://maps.google.com/?q=Northland Volkswagon',
-                        locationName: 'Northland Volkswagon',
-                        image: 'https://www.conceptcarz.com/images/Volkswagen/2011-Volkswagen-Golf-GTi-Edition-35-01.jpg'
-                    },
-                    { 
-                        title: 'Ford Focus RS', 
-                        price: '$30,000',
-                        kilometres: '0',
-                        fuelType: 'Gas',
-                        transmission: 'Manual',
-                        locationLink: 'http://maps.google.com/?q=Northstar Ford',
-                        locationName: 'Northstar Ford',
-                        image: 'https://icdn-3.motor1.com/images/mgl/J2jkJ/s1/2016-ford-focus-rs.jpg'
-                    },
-                    { 
-                        title: 'Dodge Ram', 
-                        price: '$30,000',
-                        kilometres: '12,000',
-                        fuelType: 'Diesel',
-                        transmission: 'Auto',
-                        locationLink: 'http://maps.google.com/?q=Calgary Ford',
-                        locationName: 'Calgary Ford',
-                        image: 'https://images5.alphacoders.com/892/892407.jpg'
-                    },
-                    { 
-                        title: 'Nissan Sentra Spec-V', 
-                        price: '$40,000',
-                        kilometres: '0',
-                        fuelType: 'Gas',
-                        transmission: 'Manual',
-                        locationLink: 'http://maps.google.com/?q=Cochrane Nissan',
-                        locationName: 'Cochrane Nissan',
-                        image: 'https://i.pinimg.com/originals/c2/57/b8/c257b847e55fd0572fb035ec04bb3a1e.jpg'
-                    }
-                ]
             }
         },
-
         beforeMount() {
-            this.items.sort(function(a, b){return 0.5 - Math.random()})
+            this.carDetails.sort(function(a, b){return 0.5 - Math.random()})
         },
-
         methods: {}
     }
 </script>

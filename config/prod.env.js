a4f00ef466f13f6b735b24f35ef4994d758056de
Farzam Noori <farzamn80@gmail.com>
2018-06-19 14:10:55 -0600

    Added google bucket
    Added google bucket to prod.env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    API_ROUTE: '"https://yycauto-api.herokuapp.com/"'
+    API_ROUTE: '"https://yycauto-api.herokuapp.com/"',
+    GOOGLE_BUCKET: '"https://storage.cloud.google.com/yycauto/uploads/"'
 }

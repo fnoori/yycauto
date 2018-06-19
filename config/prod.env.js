70bed55a2ff77889e583c24099526537b821c3ae
Farzam Noori <farzamn80@gmail.com>
2018-06-19 14:03:22 -0600

    Revert "Updated prod.env to include google bucket route"
    This reverts commit 0ae5e9c90f63925e22fbd269d5b06fc5375677de.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,5 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    API_ROUTE: '"https://yycauto-api.herokuapp.com/"',
-    GOOGLE_BUCKET: '"https://storage.cloud.google.com/yycauto/uploads/"'
+    API_ROUTE: '"https://yycauto-api.herokuapp.com/"'
 }

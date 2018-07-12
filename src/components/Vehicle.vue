cde571d186f34af77166abefb40cf3f976ba8215
Farzam Noori <farzamn80@gmail.com>
2018-07-12 14:25:02 -0600

    Testing bootstrap-vue carousel

--- src/components/Vehicle.vue
+++ src/components/Vehicle.vue
@@ -1,14 +1,50 @@
 <template>
   <div class="vehicle">
-    <div v-if="dataReady">
-      <carousel :per-page="1" paginationSize=5>
-        <slide v-for="photo in vehicleDetails.vehicle.VehiclePhotos" v-bind:key="photo.message">
-          <img 
-            :src="apiRoute + 'dealerships/' + vehicleDetails.vehicle.Dealership.name.split(' ').join('_') + '/' + vehicleDetails.vehicle._id + '/' + photo"
-            class="carousel-img"/>
-        </slide>
-      </carousel>
-    </div>
+    <b-carousel id="carousel1"
+                style="text-shadow: 1px 1px 2px #333;"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+    >
+
+      <!-- Text slides with image -->
+      <b-carousel-slide caption="First slide"
+                        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58">
+      </b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             src="https://picsum.photos/1024/480/?image=55" alt="image slot">
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
+          ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+
+    </b-carousel>
   </div>
 </template>
 
@@ -33,7 +69,6 @@ export default {
       .get(process.env.API_ROUTE + `vehicles/byId/${this.vehicleId}`)
       .then(response => {
         this.vehicleDetails = response.data
-        console.log(this.vehicleDetails)
         this.$nextTick(() => this.afterDataLoad())
       })
       .catch(err => {
@@ -51,10 +86,20 @@ export default {
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
   .vehicle {
-    width: 30rem;
-    margin: 0 auto; 
+    width: 55vw;
+    margin: 1rem auto;
   }
   .carousel-img {
-    width: 28rem !important
+    width: 28rem !important;
+    padding: 0 0.5rem;
+  }
+  .title h4 div {
+    display: inline-block;
+  }
+  .title .name {
+    font-weight: bold;
+  }
+  .title .price {
+    color: #70A036;
   }
 </style>

76e35a55972a27b4f778feaa7a54d9f0aeed3af6
Farzam Noori <fnoori431@gmail.com>
2019-01-07 15:08:26 -0700

    fresh start

--- src/auth/auth0-variables.js
+++
@@ -1,7 +0,0 @@
-export const AUTH_CONFIG = {
-  clientId: 'kFeNdeqy6SgySBXzN6SOSBjyn8lzw52U',
-  domain: 'yyc-automotives.auth0.com',
-  callbackUrl: 'http://localhost:8080/callback',
-  apiUrl: 'https://yyc-automotives-auth0-api',
-  audience: 'https://yyc-automotives-auth0-api'
-}
--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-export default {
-  isAuthenticated: state => {
-    const expiresAt = JSON.parse(localStorage.getItem('expires_at'))
-    return new Date().getTime() < expiresAt
-  }
-}

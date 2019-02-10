a16e6e2d0a801e2886f18fa4c7bf0de0388dd5a0
Farzam Noori <fnoori431@gmail.com>
2019-02-09 20:52:43 -0700

    Search bar and logo are responsive

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,24 @@
         </b-input-group>
       </b-nav-form>
 
-
-
-
+      <b-button
+        v-if="authenticated"
+        class="ml-auto sign-out-btn">Logout</b-button>
 
     </b-navbar>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      authenticated: false
+    }
+  }
+}
+</script>
 
 <style>
   @import url('https://fonts.googleapis.com/css?family=Comfortaa');
@@ -35,49 +45,76 @@
     background-color: #f44336 !important;
   }
 
-  /* bootstrap-vue overwritten rules */
-  .navbar form {
+  nav div,
+  nav button,
+  nav input {
+    border-radius: 0 !important;
+  }
 
+  /* bootstrap-vue overwritten rules */
+  .navbar-brand {
+    margin-right: 0 !important;
+  }
+  .navbar-brand img {
+    width: 8rem;
+    margin: -0.5rem 0rem;
   }
   .navbar form input {
     width: 50vw !important;
-    border-radius: 0;
     box-shadow: 0px 1px 1px 0px rgba(0,0,0,0.1);
   }
-  .navbar .form-control {
+  .navbar form .form-control {
     border: none;
   }
-  .navbar .form-control:focus {
+  .navbar form .form-control:focus {
     box-shadow: none;
     z-index: 0 !important;
     box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.1);
   }
-  .navbar .input-group-append {
+  .navbar form .input-group-append {
     margin-left: 1px;
   }
-  .navbar .btn {
-    border-radius: 0;
+  .navbar form .btn {
     border: 0;
     box-shadow: 0px 1px 1px 0px rgba(0,0,0,0.1);
     color: #f44336;
     background-color: white;
   }
-  .navbar .btn:hover {
+  .navbar form .btn:hover {
     color: #f44336;
     background-color: white;
     box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.1);
   }
-  .navbar .btn-secondary:active {
+  .navbar form .btn-secondary:active {
     box-shadow: 0px 0px 2px 3px rgba(0,0,0,0.1) !important;
     background-color: white !important;
     border: none !important;
     color: #f44336 !important;
   }
 
-  .navbar-brand {
-    margin-right: 0;
+  /* custom css */
+  .sign-out-btn {
+    background-color: white !important;
+    border: none !important;
+    color: red !important;
+    font-weight: bold !important;
+    font-size: .8em !important;
+    border-radius: 1rem !important;
+    box-shadow: 0px 1px 1px 0px rgba(0,0,0,0.1) !important;
   }
-  .navbar-brand img {
-    width: 6rem;
+  .sign-out-btn:hover {
+    box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.1) !important;
+  }
+  .sign-out-btn:active {
+    box-shadow: 0px 0px 2px 3px rgba(0,0,0,0.1) !important;
+  }
+
+  @media (max-width: 485px) {
+    /* bootstrap-vue overwritten rules */
+
+    .navbar form {
+      margin: 1rem 0 0 0 !important;
+      width: 100vw;
+    }
   }
 </style>

7cc8214269dd5ce2564a3ee23675d623b04f5678
Farzam Noori <fnoori431@gmail.com>
2019-02-26 12:08:23 -0700

    will be using 2 different ways to show details of vehicle
    on big screens, three separate columns
    for smaller screens using pills

--- src/components/RelatedVehicles.vue
+++ src/components/RelatedVehicles.vue
@@ -1,20 +1,11 @@
 <template>
-  <div class="related-vehicles">
-    from related vehicles
-  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'TierOne',
-  data() {
-
-  }
 }
 </script>
 
 <style scoped>
-  .related-cars {
-
-  }
 </style>
--- src/views/Vehicle.vue
+++ src/views/Vehicle.vue
@@ -53,7 +53,139 @@
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </div>
 
-    <div class="details">
+    <div class="details-cols">
+      <div class="details-basic">
+        <div class="details-title font-weight-bold">Basic</div>
+        <b-list-group>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Make
+            </div>
+            <div class="details-cols-value">
+              Honda
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Model
+            </div>
+            <div class="details-cols-value">
+              Civic
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Trim
+            </div>
+            <div class="details-cols-value">
+              SE
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Kilometres
+            </div>
+            <div class="details-cols-value">
+              23,000
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Fuel Type
+            </div>
+            <div class="details-cols-value">
+              Gas
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Seats
+            </div>
+            <div class="details-cols-value">
+              5
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              CarProof
+            </div>
+            <div class="details-cols-value">
+              No
+            </div>
+          </b-list-group-item>
+        </b-list-group>
+      </div>
+      <div class="details-mechanical">
+        <div class="mechanical-title font-weight-bold">Mechanical</div>
+        <b-list-group>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Transmission
+            </div>
+            <div class="details-cols-value">
+              Auto
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Engine (L)
+            </div>
+            <div class="details-cols-value">
+              1.8
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Torque
+            </div>
+            <div class="details-cols-value">
+              120 @ 5,000
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Fuel Grade
+            </div>
+            <div class="details-cols-value">
+              Regular
+            </div>
+          </b-list-group-item>
+        </b-list-group>
+      </div>
+      <div class="details-environmental">
+        <div class="environmental-title font-weight-bold">Environmental</div>
+        <b-list-group>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              City
+            </div>
+            <div class="details-cols-value">
+              7.8
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Highway
+            </div>
+            <div class="details-cols-value">
+              8.4
+            </div>
+          </b-list-group-item>
+          <b-list-group-item class="details-cols-row">
+            <div class="details-cols-title">
+              Combined
+            </div>
+            <div class="details-cols-value">
+              8.0
+            </div>
+          </b-list-group-item>
+        </b-list-group>
+      </div>
+
+    </div>
+
+    <!-- pills only visible on mobile/smaller screens -->
+    <div class="details details-pills">
       <b-tabs pills v-model="tabIndex">
         <b-tab title="Basic" :title-link-class="linkClass(0)">
           <b-list-group class="vehicle-details">
@@ -154,21 +286,14 @@
       </b-tabs>
     </div>
 
-    <related-vehicles class="related">
-    </related-vehicles>
-
   </div>
 </template>
 
 <script>
   import axios from 'axios'
-  import RelatedVehicles from '../components/RelatedVehicles'
 
   export default {
     name: 'vehicle',
-    components: {
-      'related-vehicles': RelatedVehicles
-    },
     data() {
       return {
         tabIndex: 0
@@ -241,32 +366,70 @@
     color: rgba(0,0,0,0.6);
   }
 
-  .details {
-    padding: 0.5rem;
-    width: 30rem;
-    margin: 1rem auto;
+
+  .details-pills {
+    display: none;
   }
-  .details .vehicle-details {
-    margin-top: 1rem;
-  }
-  .details .title {
+  .details-cols {
     display: inline-block;
+    width: 100%;
+    margin: 0 5%;
+  }
+  .details-cols .details-basic {
+    width: 33.33%;
+    display: inline-block;
+  }
+  .details-cols .details-mechanical {
+    width: 33.33%;
+    display: inline-block;
+  }
+  .details-cols .details-environmental {
+    width: 33.33%;
+    display: inline-block;
+  }
+  .details-cols .details-cols-row {
+    display: inline-flex;
+  }
+
+  .details-cols .details-cols-title {
     width: 50%;
     color: rgba(0,0,0,0.6);
     font-size: 0.9rem;
   }
-  .details .value {
-    display: inline-block;
+  .details-cols .details-cols-value {
+    width: 50%;
     font-size: 0.9rem;
-    width: 50%;
-  }
-  .list-contents {
-    margin: 0 auto;
-    width: 50%;
   }
 
-  .related-vehicles {
-
+  @media (max-width: 830px) {
+    .details-pills {
+      display: block;
+    }
+    .details-cols {
+      display: none;
+    }
+    .details {
+      width: 80%;
+      margin: 0 auto;
+    }
+    .list-contents {
+      margin: 0 auto;
+      width: 70%;
+    }
+    .details .vehicle-details {
+      margin-top: 1rem;
+    }
+    .details .title {
+      display: inline-block;
+      width: 50%;
+      color: rgba(0,0,0,0.6);
+      font-size: 0.9rem;
+    }
+    .details .value {
+      display: inline-block;
+      font-size: 0.9rem;
+      width: 50%;
+    }
   }
 
   @media (max-width: 630px) {

d5e356a687b6051b6832cff7d9d696bfd7af3b6f
Farzam Noori <fnoori431@gmail.com>
2019-04-06 13:29:52 -0600

    Using Buefy + Bulma as css frameworks
    Using scss as css pre processor
    
    Created bulma theme customization file
    
    Implemented generic header

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Buefy from 'buefy'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fa'
+})
 
 new Vue({
   router,
